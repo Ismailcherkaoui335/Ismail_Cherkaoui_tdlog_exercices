3460a1bb557e73512c007b7129b0434513658bdd
Ismailcherkaoui335 <114833012+Ismailcherkaoui335@users.noreply.github.com>
2023-10-21 05:44:34 +0100

    Update exo2.py

--- exo2/exo2.py
+++ exo2/exo2.py
@@ -2,6 +2,37 @@ def solution(str1,str2):
     a = len(str1)
     b = len(str2)
     return str1[slice(a-b,a)] == str2
+
+fixed_tests_True = (
+    ("samurai", "ai"),
+    ("ninja", "ja"),
+    ("sensei", "i"),
+    ("abc", "abc"),
+    ("abcabc", "bc"),
+    ("fails", "ails"),
+)
+
+fixed_tests_False = (
+    ("sumo", "omo"),
+    ("samurai", "ra"),
+    ("abc", "abcd"),
+    ("ails", "fails"),
+    ("this", "fails"),
+    ("spam", "eggs"),
+)
+
+for test_case in fixed_tests_True:
+    input_str, suffix = test_case
+    result = solution(input_str, suffix)
+    assert result is True, f"Expected True for input {input_str} and suffix {suffix}, but got {result}"
+
+for test_case in fixed_tests_False:
+    input_str, suffix = test_case
+    result = solution(input_str, suffix)
+    assert result is False, f"Expected False for input {input_str} and suffix {suffix}, but got {result}"
+
+print("All tests passed!")
+
     
 
 
